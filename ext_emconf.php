5721b70a390c245dd17bde7899e3125de808a884
Mathias Brodala <mbrodala@pagemachine.de>
2018-10-11 12:16:15 +0200

    [TASK] Create fgtclb/typo3-oauth2-server package

---
+++ ext_emconf.php
@@ -0,0 +1,16 @@
+<?php declare(strict_types = 1);
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'TYPO3 OAuth2 server',
+    'description' => 'OAuth2 server implementation for TYPO3 frontend users',
+    'category' => 'fe',
+    'state' => 'alpha',
+    'author' => 'FGTCLB',
+    'author_email' => 'info@fgtclb.com',
+    'version' => '0.0.0',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '9.5.0-9.5.99',
+            'frontend' => '9.5.0',
+        ],
+    ],
+];
