4a90b4130b62433d28d9aa90c3c58e52d05463be
Mathias Brodala <mbrodala@pagemachine.de>
2018-10-15 17:04:21 +0200

    [TASK] Redirect to login

--- Classes/Middleware/OAuth2Authorization.php
+++ Classes/Middleware/OAuth2Authorization.php
@@ -4,13 +4,17 @@ declare(strict_types = 1);
 namespace FGTCLB\OAuth2Server\Middleware;
 
 use FGTCLB\OAuth2Server\Server\ServerFactory;
+use FGTCLB\OAuth2Server\Session\UserSession;
 use League\OAuth2\Server\Exception\OAuthServerException;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\MiddlewareInterface;
 use Psr\Http\Server\RequestHandlerInterface;
 use TYPO3\CMS\Core\Context\Context;
+use TYPO3\CMS\Core\Http\RedirectResponse;
 use TYPO3\CMS\Core\Http\Response;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Frontend\ContentObject\ContentObjectRenderer;
 
 /**
  * Handler for OAuth2 authorization requests
@@ -30,29 +34,72 @@ final class OAuth2Authorization implements MiddlewareInterface
         $factory = new ServerFactory();
         $server = $factory->buildAuthorizationServer();
 
-        try {
-            $authorizationRequest = $server->validateAuthorizationRequest($request);
+        $userSession = new UserSession();
+        $authorizationRequest = $userSession->getData('oauth2.authorizationRequest');
 
-            /** @var Context */
-            $context = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(Context::class);
-
-            if (!$context->getPropertyFromAspect('frontend.user', 'isLoggedIn', false)) {
-                // \TYPO3\CMS\Extbase\Utility\DebuggerUtility::var_dump($GLOBALS['TSFE']->fe_user, __METHOD__, 8, defined('TYPO3_cliMode') || defined('TYPO3_REQUESTTYPE') && (TYPO3_REQUESTTYPE & TYPO3_REQUESTTYPE_CLI));
-                throw new \Exception("Error Processing Request", 1);
+        if (!$authorizationRequest) {
+            try {
+                $authorizationRequest = $server->validateAuthorizationRequest($request);
+            } catch (OAuthServerException $e) {
+                return $e->generateHttpResponse(new Response());
             }
+        }
 
-            $user = new class implements \League\OAuth2\Server\Entities\UserEntityInterface {
-                use \League\OAuth2\Server\Entities\Traits\EntityTrait;
-            };
-            $user->setIdentifier('test');
-            $authorizationRequest->setUser($user);
-            $authorizationRequest->setAuthorizationApproved(true);
+        // Ensure FE user groups are loaded and URLs can be generated
+        $this->bootFrontendController();
 
+        /** @var Context */
+        $context = GeneralUtility::makeInstance(Context::class);
+
+        if (!$context->getPropertyFromAspect('frontend.user', 'isLoggedIn', false)) {
+            $userSession->setData('oauth2.authorizationRequest', $authorizationRequest);
+
+            /** @var ContentObjectRenderer */
+            $contentObjectRenderer = GeneralUtility::makeInstance(ContentObjectRenderer::class);
+            $redirectUri = $contentObjectRenderer->typoLink_URL([
+                'parameter' => sprintf('t3://page?uid=%d&redirect_url=%s', 1, '/oauth/authorize'),
+            ]);
+
+            return new RedirectResponse($redirectUri);
+        }
+
+        $user = new class implements \League\OAuth2\Server\Entities\UserEntityInterface {
+            use \League\OAuth2\Server\Entities\Traits\EntityTrait;
+        };
+        $user->setIdentifier('test');
+        $authorizationRequest->setUser($user);
+        $authorizationRequest->setAuthorizationApproved(true);
+
+        $userSession->removeData('oauth2.authorizationRequest');
+
+        try {
             return $server->completeAuthorizationRequest($authorizationRequest, new Response());
         } catch (OAuthServerException $e) {
             return $e->generateHttpResponse(new Response());
         }
 
         return (new Response())->withStatus(500);
+    }
+
+    /**
+     * Finishing booting up TSFE, after that the following properties are available:
+     *
+     * - TSFE->fe_user
+     * - TSFE->sys_page
+     * - TSFE->tmpl
+     * - TSFE->config
+     * - TSFE->cObj
+     *
+     * So a link to a page could be generated.
+     */
+    protected function bootFrontendController()
+    {
+        // disable page errors
+        $GLOBALS['TYPO3_CONF_VARS']['FE']['pageUnavailable_handling'] = false;
+        $GLOBALS['TSFE']->fetch_the_id();
+        $GLOBALS['TSFE']->getConfigArray();
+        $GLOBALS['TSFE']->settingLanguage();
+        $GLOBALS['TSFE']->settingLocale();
+        $GLOBALS['TSFE']->newCObj();
     }
 }
---
+++ Classes/Session/UserSession.php
@@ -0,0 +1,55 @@
+<?php
+declare(strict_types = 1);
+
+namespace FGTCLB\OAuth2Server\Session;
+
+use TYPO3\CMS\Frontend\Authentication\FrontendUserAuthentication;
+
+/**
+ * Wrapper for the frontend user session
+ */
+final class UserSession
+{
+    /**
+     * @var FrontendUserAuthentication
+     */
+    protected $frontendUser;
+
+    public function __construct(FrontendUserAuthentication $frontendUser = null)
+    {
+        $this->frontendUser = $frontendUser ?: $GLOBALS['TSFE']->fe_user;
+    }
+
+    /**
+     * Retrieve data from the user session
+     *
+     * @param string $key
+     * @return mixed
+     */
+    public function getData(string $key)
+    {
+        return $this->frontendUser->getSessionData($key);
+    }
+
+    /**
+     * Store data in the user session
+     *
+     * @param string $key
+     * @param mixed $data
+     */
+    public function setData(string $key, $data): void
+    {
+        $this->frontendUser->setAndSaveSessionData($key, $data);
+    }
+
+    /**
+     * Store data in the user session
+     *
+     * @param string $key
+     * @param mixed $data
+     */
+    public function removeData(string $key): void
+    {
+        $this->frontendUser->setAndSaveSessionData($key, null);
+    }
+}
