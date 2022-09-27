7fb6a98f6884e7e39aedd7bda48d5a7ba6a83139
Oleksandr Matsko <Oleksandr.Matsko@sitegeist.de>
2022-09-27 17:32:49 +0200

    [TASK] #73408 Make compatable with Typo3 11.5

--- ext_emconf.php
+++ ext_emconf.php
@@ -9,8 +9,8 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '0.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '9.5.0-10.99.99',
-            'frontend' => '9.5.0-10.99.99',
+            'typo3' => '10.4.0-11.5.99',
+            'frontend' => '10.4.0-11.5.99',
         ],
     ],
 ];
