966588d81d89532855b6fa80834044e19166fc79
Andreas Wolf <dev@a-w.io>
2022-12-13 12:23:25 +0100

    Move client secret verification into Client Entity
    This also changes the repository to use the client entity itself where
    possible. The entity instances are created using a factory method, which
    will in the future ease changes in the table's schema.

--- Classes/Domain/Entity/Client.php
+++ Classes/Domain/Entity/Client.php
@@ -3,23 +3,53 @@ declare(strict_types = 1);
 
 namespace FGTCLB\OAuth2Server\Domain\Entity;
 
+use FGTCLB\OAuth2Server\Domain\Repository\ClientRepository;
 use League\OAuth2\Server\Entities\ClientEntityInterface;
 use League\OAuth2\Server\Entities\Traits\ClientTrait;
 use League\OAuth2\Server\Entities\Traits\EntityTrait;
+use TYPO3\CMS\Core\Crypto\PasswordHashing\PasswordHashFactory;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
 
 /**
  * OAuth2 client
+ *
+ * @phpstan-import-type TClientRow from ClientRepository
  */
 final class Client implements ClientEntityInterface
 {
     use EntityTrait;
     use ClientTrait;
 
-    public function __construct($identifier, $name, $redirectUri, $isConfidential)
+    private string $secret;
+
+    private function __construct(string $identifier, string $name, string $redirectUri, string $secret, bool $isConfidential)
     {
         $this->identifier = $identifier;
         $this->name = $name;
-        $this->redirectUri = $redirectUri;
+        $this->redirectUri = GeneralUtility::trimExplode("\n", $redirectUri);
+        $this->secret = $secret;
         $this->isConfidential = $isConfidential;
+    }
+
+    /**
+     * @param TClientRow $row
+     */
+    public static function fromDatabaseRow(array $row): self
+    {
+        return new self(
+            $row['identifier'],
+            $row['name'],
+            $row['redirect_uris'],
+            $row['secret'],
+            // TODO make this configurable once we support OAuth2 e.g. in a browser, where the client secret cannot be kept secret
+            true
+        );
+    }
+
+    public function validateSecret(string $secret, PasswordHashFactory $hashFactory): bool
+    {
+        return $hashFactory
+            ->get($this->secret, 'FE')
+            ->checkPassword($secret, $this->secret);
     }
 }
