bdcc4c9a8d8dd3510b3e3870f7976ca96ff9220a
Andreas Wolf <dev@a-w.io>
2022-12-13 12:23:25 +0100

    Move client secret verification into Client Entity
    This also changes the repository to use the client entity itself where
    possible. The entity instances are created using a factory method, which
    will in the future ease changes in the table's schema.

--- Classes/Domain/Repository/ClientRepository.php
+++ Classes/Domain/Repository/ClientRepository.php
@@ -3,18 +3,22 @@ declare(strict_types = 1);
 
 namespace FGTCLB\OAuth2Server\Domain\Repository;
 
+use Doctrine\DBAL\Result;
 use FGTCLB\OAuth2Server\Domain\Entity\Client;
-use League\OAuth2\Server\Entities\ClientEntityInterface;
 use League\OAuth2\Server\Repositories\ClientRepositoryInterface;
 use Psr\Log\LoggerAwareInterface;
 use Psr\Log\LoggerAwareTrait;
 use TYPO3\CMS\Core\Crypto\PasswordHashing\PasswordHashFactory;
+use TYPO3\CMS\Core\Database\Connection;
 use TYPO3\CMS\Core\Database\ConnectionPool;
-use TYPO3\CMS\Core\Database\Query\QueryBuilder;
-use TYPO3\CMS\Core\Utility\GeneralUtility;
 
 /**
- * Repository for OAuth2 clients
+ * Repository for OAuth2 clients.
+ *
+ * This does not follow TYPO3's usual naming conventions for repositories because it implements an interface from
+ * league/oauth2-server.
+ *
+ * @phpstan-type TClientRow array{uid: int, identifier: string, name: string, secret: string, redirect_uris: string, description: string}
  */
 final class ClientRepository implements ClientRepositoryInterface, LoggerAwareInterface
 {
@@ -22,84 +26,67 @@ final class ClientRepository implements ClientRepositoryInterface, LoggerAwareIn
 
     private const TABLE_NAME = 'tx_oauth2server_client';
     private PasswordHashFactory $hashFactory;
-    private QueryBuilder $queryBuilder;
-    private array $clientData;
+    private Connection $databaseConnection;
 
     public function __construct(PasswordHashFactory $hashFactory, ConnectionPool $connectionPool)
     {
         $this->hashFactory = $hashFactory;
-        $this->queryBuilder = $connectionPool->getQueryBuilderForTable(self::TABLE_NAME);
+        $this->databaseConnection = $connectionPool->getConnectionForTable(self::TABLE_NAME);
     }
 
     /**
-     * Get a client.
-     *
-     * @param string $clientIdentifier The client's identifier
-     * @param string|null $grantType The grant type used (if sent)
-     * @param string|null $clientSecret The client's secret (if sent)
-     * @param bool $mustValidateSecret If true the client must attempt to validate the secret if the client
-     *                                        is confidential
-     *
-     * @return ClientEntityInterface
+     * @inheritDoc
      */
-    public function getClientEntity($clientIdentifier, $grantType = null, $clientSecret = null, $mustValidateSecret = true)
+    public function getClientEntity($clientIdentifier): ?Client
     {
-        // not all steps in OAuth2 supply a client secret, so we only validate it if it is supplied
-        if ($mustValidateSecret && $clientSecret !== null && !$this->validateClient($clientIdentifier, $clientSecret, $grantType)) {
-            $this->logger->debug(sprintf('Submitted secret "%s" is invalid for client %s', $clientSecret, $clientIdentifier));
-
-            return null;
-        }
-
         $clientData = $this->findRawByIdentifier($clientIdentifier);
         if (!$clientData) {
             return null;
         }
 
-        return new Client(
-            $clientIdentifier,
-            $clientData['name'],
-            GeneralUtility::trimExplode("\n", $clientData['redirect_uris']),
-            true // todo: make configurable: $clientData['confidential']
-        );
-    }
-
-    protected function findRawByIdentifier(string $clientIdentifier): ?array
-    {
-        return $this->queryBuilder
-            ->resetQueryParts()
-            ->select('*')
-            ->from(self::TABLE_NAME)
-            ->where($this->queryBuilder->expr()->eq(
-                'identifier',
-                $this->queryBuilder->createNamedParameter($clientIdentifier)
-            ))
-            ->setMaxResults(1)
-            ->execute()
-            ->fetchAssociative() ?: null;
+        return Client::fromDatabaseRow($clientData);
     }
 
     /**
-     * Validate a client's secret.
-     *
-     * @param string $clientIdentifier The client's identifier
-     * @param null|string $clientSecret The client's secret (if sent)
-     * @param null|string $grantType The type of grant the client is using (if sent)
-     *
-     * @return bool
+     * @return TClientRow|null
+     */
+    protected function findRawByIdentifier(string $clientIdentifier): ?array
+    {
+        $queryBuilder = $this->databaseConnection
+            ->createQueryBuilder();
+        /** @var Result $result select() always yields a Result instance and not an int */
+        $result = $queryBuilder
+            ->select('*')
+            ->from(self::TABLE_NAME)
+            ->where(
+                $queryBuilder->expr()->eq('identifier', $queryBuilder->createNamedParameter($clientIdentifier))
+            )
+            ->setMaxResults(1)
+            ->execute();
+        /** @var TClientRow|false $row */
+        $row = $result->fetchAssociative();
+        return $row ?: null;
+    }
+
+    /**
+     * @inheritDoc
      */
     public function validateClient($clientIdentifier, $clientSecret, $grantType)
     {
+        // we currently only support confidential clients, which must always supply a secret (TODO check if this is actually true).
         if ($clientSecret === null) {
             return false;
         }
 
-        $clientData = $this->findRawByIdentifier($clientIdentifier);
-        if (!$clientData) {
+        $client = $this->getClientEntity($clientIdentifier);
+        if ($client === null) {
             return false;
         }
 
-        $hash = $this->hashFactory->get($clientData['secret'], 'FE');
-        return $hash->checkPassword($clientSecret, $clientData['secret']);
+        $result = $client->validateSecret($clientSecret, $this->hashFactory);
+        if ($result === false) {
+            $this->logger->debug(sprintf('Submitted secret "%s" is invalid for client %s', $clientSecret, $clientIdentifier));
+        }
+        return $result;
     }
 }
