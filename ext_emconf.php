5c3d409a23fd0bd47d04d48e2ad511c7f15f5831
mhofmann <m.hofmann@web-vision.de>
2024-03-25 11:01:04 +0100

    [TASK] Run CS Fixer
    command:
    `Build/Scripts/runTests.sh -s cgl`

--- ext_emconf.php
+++ ext_emconf.php
@@ -9,8 +9,8 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '1.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '10.4.0-11.5.99',
-            'frontend' => '10.4.0-11.5.99',
+            'typo3' => '11.5.0-11.5.99',
+            'frontend' => '11.5.0-11.5.99',
         ],
     ],
 ];
