c6142ca0a7383f77165d046e5bf43583996a7c31
mhofmann <m.hofmann@web-vision.de>
2024-04-05 16:47:49 +0200

    [TASK] Add oAuth2 authorization tests
    * adds successful login redirect test with logged-in user
    * adds login redirect to login page if no logged-in user

---
+++ Tests/Functional/Extensions/testsuite/Classes/TestCase/AbstractSiteCest.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace FGTCLB\Testsuite\TestCase;
+
+use FGTCLB\Testsuite\Traits\SiteBasedTestTrait;
+use TYPO3\CMS\Core\Crypto\PasswordHashing\Argon2iPasswordHash;
+use TYPO3\TestingFramework\Core\Functional\FunctionalTestCase;
+
+class AbstractSiteCest extends FunctionalTestCase
+{
+    use SiteBasedTestTrait;
+
+    protected const LANGUAGE_PRESETS = [];
+
+    protected const BASE_URL = 'https://localhost/';
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $GLOBALS['TYPO3_CONF_VARS']['SYS']['encryptionKey'] = '12345';
+        $GLOBALS['TYPO3_CONF_VARS']['BE']['passwordHashing']['className'] = Argon2iPasswordHash::class;
+
+        $this->writeSiteConfiguration(
+            'fcp-local',
+            $this->buildSiteConfiguration(
+                1,
+                self::BASE_URL,
+                'FES Community-Portal'
+            )
+        );
+    }
+}
