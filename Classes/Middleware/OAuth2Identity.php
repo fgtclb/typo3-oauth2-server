f635a4e64c9663862cf3dbe48bda183af08cf4e2
mhofmann <m.hofmann@web-vision.de>
2024-04-08 21:14:47 +0200

    [FEATURE] Make endpoints and lifetimes configurable
    The current implementation has hard-coded OAuth endpoints. With this
    commit endpoints for authorization, token generation and refreshing and
    resource handling are made dynamic via Extension configuration.
    Additionally, the lifetimes of tokens and authCode are made dynamic with
    fallback to the previous defined default.

--- Classes/Middleware/OAuth2Identity.php
+++ Classes/Middleware/OAuth2Identity.php
@@ -3,6 +3,7 @@
 declare(strict_types=1);
 namespace FGTCLB\OAuth2Server\Middleware;
 
+use FGTCLB\OAuth2Server\Configuration;
 use FGTCLB\OAuth2Server\Server\ServerFactory;
 use FGTCLB\OAuth2Server\Service\ResourceHandlingFactory;
 use League\OAuth2\Server\Exception\OAuthServerException;
@@ -19,13 +20,16 @@ use TYPO3\CMS\Core\Http\Response;
  */
 final class OAuth2Identity implements MiddlewareInterface
 {
+    private Configuration $configuration;
     private ServerFactory $authServerFactory;
     private ResourceHandlingFactory $resourceServerFactory;
 
     public function __construct(
+        Configuration $configuration,
         ResourceHandlingFactory $resourceServerFactory,
         ServerFactory $authServerFactory
     ) {
+        $this->configuration = $configuration;
         $this->resourceServerFactory = $resourceServerFactory;
         $this->authServerFactory = $authServerFactory;
     }
@@ -35,27 +39,26 @@ final class OAuth2Identity implements MiddlewareInterface
      */
     public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
     {
-        if ($request->getUri()->getPath() === '/oauth/identity') {
-            $server = $this->authServerFactory->buildResourceServer();
-
-            try {
-                // OAuth Auth Server validates the request
-                $request = $server->validateAuthenticatedRequest($request);
-                $clientId = $request->getAttribute('oauth_client_id');
-
-                // Internal resource handler accepts the request
-                // and generates a response
-                //
-                // we return the resourceHandler response here, as we don't want to have the path
-                // moved to TYPO3 handling. In most cases, the route won't be able inside the TYPO3,
-                // so the handler has to decide whether to redirect or return a response.
-                $resourceHandler = $this->resourceServerFactory->getResourceHandler($clientId);
-                return $resourceHandler->handleAuthenticatedRequest($request);
-            } catch (OAuthServerException $e) {
-                return $e->generateHttpResponse(new Response());
-            }
+        if ($request->getUri()->getPath() !== $this->configuration->getResourceEndpoint()) {
+            return $handler->handle($request);
         }
+        $server = $this->authServerFactory->buildResourceServer();
 
-        return $handler->handle($request);
+        try {
+            // OAuth Auth Server validates the request
+            $request = $server->validateAuthenticatedRequest($request);
+            $clientId = $request->getAttribute('oauth_client_id');
+
+            // Internal resource handler accepts the request
+            // and generates a response
+            //
+            // we return the resourceHandler response here, as we don't want to have the path
+            // moved to TYPO3 handling. In most cases, the route won't be able inside the TYPO3,
+            // so the handler has to decide whether to redirect or return a response.
+            $resourceHandler = $this->resourceServerFactory->getResourceHandler($clientId);
+            return $resourceHandler->handleAuthenticatedRequest($request);
+        } catch (OAuthServerException $e) {
+            return $e->generateHttpResponse(new Response());
+        }
     }
 }
