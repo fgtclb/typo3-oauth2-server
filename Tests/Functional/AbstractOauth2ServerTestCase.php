fd255bf94de61ac5d1d7308171742a2bbc59f77f
mhofmann <m.hofmann@web-vision.de>
2024-04-09 13:04:03 +0200

    [TASK] Add refresh token test
    Adds Functional test for refreshing token and moves test preparation
    requests to AbstractOauth2ServerTestCase

--- Tests/Functional/AbstractOauth2ServerTestCase.php
+++ Tests/Functional/AbstractOauth2ServerTestCase.php
@@ -5,7 +5,10 @@ declare(strict_types=1);
 namespace FGTCLB\OAuth2Server\Tests\Functional;
 
 use FGTCLB\Testsuite\TestCase\AbstractSiteTestCase;
+use TYPO3\CMS\Core\Http\Uri;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\TestingFramework\Core\Functional\Framework\Frontend\InternalRequest;
+use TYPO3\TestingFramework\Core\Functional\Framework\Frontend\InternalRequestContext;
 
 abstract class AbstractOauth2ServerTestCase extends AbstractSiteTestCase
 {
@@ -47,5 +50,53 @@ abstract class AbstractOauth2ServerTestCase extends AbstractSiteTestCase
         chmod(GeneralUtility::getFileAbsFileName(self::PUBLIC_KEY_FILE_PATH), 0600);
         chmod(GeneralUtility::getFileAbsFileName(self::PRIVATE_KEY_FILE_PATH), 0600);
         $this->importCSVDataSet(__DIR__ . '/Fixtures/oauth.csv');
+    }
+
+    /**
+     * Prepares authorization and returns the authentication code.
+     *
+     * @return string The authentication code.
+     */
+    final protected function prepareAuthorizationAndReturnAuthCode(): string
+    {
+        $internalRequestContext = (new InternalRequestContext())
+            ->withFrontendUserId(self::FE_USER_ID);
+
+        $loginUri = (string)(new Uri(self::BASE_URL))
+            ->withPath('/oauth/authorize')
+            ->withQuery('?response_type=code&client_id=acme_client');
+        $loginResponse = $this->executeFrontendSubRequest(
+            (new InternalRequest($loginUri)),
+            $internalRequestContext
+        );
+        $redirectUri = $loginResponse->getHeader('Location')[0];
+        parse_str(parse_url($redirectUri, PHP_URL_QUERY) ?: '', $query);
+
+        return is_string($query['code']) ? $query['code'] : '';
+    }
+
+    /**
+     * @return array{access_token: string, refresh_token: string}
+     * @throws \JsonException
+     */
+    final protected function getGeneratedTokens(): array
+    {
+        $authCode = $this->prepareAuthorizationAndReturnAuthCode();
+        $uri = (string)(new Uri(self::BASE_URL))
+            ->withPath('/oauth/token');
+        $authBody = [
+            'grant_type' => 'authorization_code',
+            'client_id' => 'acme_client',
+            'client_secret' => 'password',
+            'code' => $authCode,
+        ];
+        $response = $this->executeFrontendSubRequest(
+            (new InternalRequest($uri))
+                ->withParsedBody($authBody)
+        );
+        $response->getBody()->rewind();
+        $responseBody = $response->getBody()->getContents();
+
+        return json_decode($responseBody, true, 512, JSON_THROW_ON_ERROR);
     }
 }
