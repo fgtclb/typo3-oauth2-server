5eaf2e2d729f6beee1d0e0531ad84b739029a9c5
Stefan Bürk <stefan@buerk.tech>
2025-06-17 12:03:51 +0200

    [TASK] Streamline identity handler implementation
    This extension ships a generic middleware implementation
    dispatching to registered custom handler implementations
    based on the `IdentityHandlerInterface`, providing base
    functionality with the `AbstractIdentityHandler`.
    
    It's considerable bad practice to pullate constructor in
    abstract classes to avoid making the constructor public
    api and falling under casual breaking policy, which has
    been ignored with the first implementation towards the
    2.x implementation. Not having a release in place, this
    is now corrected.
    
    * Add `setConfiguration(Configuration $configuration)`
      to the interface, set by the `IdentityHandlingFactory`,
      ensures that all handler implementation recives the
      OAuth2 server configuration.
    * `AbstractIdenttyHandler::__construct()` is replaced
      by implementing the new `IdentityHandlerInterface`
      method `setConfiguration()`.
    * `DefaultIdentityHandler` is used as error default and
      is now marked final, got a setter to set the client
      id and is not registered normally in the stack and
      used as default for not registered clientId responding
      with the client id in the error message to easy the
      debugging.
    * `IdentityHandlerFactory` is modified to recieve the
      OAuth configuration and set it to all handlers which
      are registered using the new interface setter.

--- Classes/Service/AbstractIdentityHandler.php
+++ Classes/Service/AbstractIdentityHandler.php
@@ -8,14 +8,22 @@ use FGTCLB\OAuth2Server\Configuration;
 
 abstract class AbstractIdentityHandler implements IdentityHandlerInterface
 {
-    protected string $clientId = '';
     /**
      * @var string[]
      */
     protected array $scopes = [];
-    protected Configuration $configuration;
 
-    public function __construct(Configuration $configuration)
+    protected ?Configuration $configuration = null;
+
+    /**
+     * Ensure that configuration can be passed and made available
+     * in all handler implementation as enforcing constructor in
+     * interfaces are discouraged. Same counts eventually provided
+     * abstract implementations to leave constructor conflict free.
+     *
+     * {@see IdentityHandlingFactory::addIdentityHandler()}
+     */
+    public function setConfiguration(Configuration $configuration): void
     {
         $this->configuration = $configuration;
     }
